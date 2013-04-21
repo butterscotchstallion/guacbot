d19ccbb6c482ca24cf4803841e5b6ebc6349a540
Your Name <bill@prgmrbill.com>
2013-04-20 21:26:59 -0400

    Adjusts plugins to only activate when the bot is being addressed directly rather than using commands like !uptime
    Moved bot.js into lib directly

--- plugins/caps-lock-saturday.js
+++ plugins/caps-lock-saturday.js
@@ -9,35 +9,69 @@ var cls    = { };
 var moment = require('moment');
 
 cls.init = function (client) {
-    client.addListener('message#', function (from, to, message) {
-        if (message.toLowerCase() === '!cls') {
-            cls.capitalizeNick(client, client.config.nick);
+    // User changes nick
+    client.addListener('nick', function (oldNick, newNick, channels, message) {
+        var clen = channels.length;
+        
+        for (var j = 0; j < clen; j++) {
+            cls.voiceUserIfNickIsUppercase(client, channels[j], newNick);
         }
     });
     
-    client.addListener('names#nodebot', function (names) {
+    // User joins channel
+    client.addListener('join', function (channel, nick, message) {
+        if (nick !== client.config.nick && cls.isSaturday()) {
+            cls.voiceUserIfNickIsUppercase(client, channel, nick);
+        }
+    });
+    
+    // Received user list from channel
+    client.addListener('names#', function (channel, names) {
+        console.log('Received NAMES');
         console.log(names);
+        
+        var nlen       = names.length;
+        var namesArray = [];
+        
+        for (var nick in names) {
+            namesArray.push(nick);
+        }
+        
+        client.say('#' + channel, namesArray.join(' '));
     });
     
     // Each time there is a ping, check if it's saturday
     // and if so, change nick
     client.addListener('ping', function () {
-        var dayOfWeek = moment().day();
-        var saturday  = 6;
-        
-        if (dayOfWeek === saturday) {
-            // Change nick if it's Saturday
+        if (cls.isSaturday()) {
+            // CLS! uppercase nick
             cls.capitalizeNick(client, client.config.nick);
-            
-            // Voice anyone who has an uppercase nick
-            // TODO: maybe this should happen for each channel in config
-            //client.send('NAMES', '#nodebot');
-            
         } else {
             // If it's not saturday, lowercase nick
             client.send('NICK', client.config.nick.toLowerCase());
         }
     });
+};
+
+cls.voiceUserIfNickIsUppercase = function (client, channel, nick) {
+    // if nick is capitalized, voice
+    if (cls.isNickUpperCase(nick)) {
+        console.log(nick + 'is capitalized');
+        cls.voiceUser(client, channel, nick);
+    } else {
+        console.log(nick + 'is not capitalized');
+    }
+};
+
+cls.voiceUser = function (client, channel, nick) {
+    client.send('MODE', channel, '+v', nick);
+};
+
+cls.isSaturday = function () {
+    var dayOfWeek = moment().day();
+    var saturday  = 6;
+    
+    return dayOfWeek === saturday;
 };
 
 cls.isNickUpperCase = function (nick) {
--- plugins/uptime.js
+++ plugins/uptime.js
@@ -4,12 +4,16 @@
  */
 "use strict";
 
+var parser = require('../lib/messageParser');
 var moment = require('moment');
 var uptime = { };
 
 uptime.init = function (client) {
     client.addListener('message#', function (from, to, message) {
-        if (message === '!uptime') {
+        var words           = parser.splitMessageIntoWords(message);
+        var isAddressingBot = parser.isMessageAddressingBot(message, client.config.nick);
+        
+        if (isAddressingBot && words[1] === 'uptime') {
             uptime.getUptime(client.connectTime, function (uptime) {
                 client.say(to, uptime);
             });
