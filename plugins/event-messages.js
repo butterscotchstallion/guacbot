9a054f9c94d859ab048e988621bf60b3759f9a1c
Your Name <bill@prgmrbill.com>
2013-05-15 02:12:32 -0400

    Adds variable replacement for nick, channel, reason, by (person who kicked you) and kick message Improves replacement strategy - now using global regex to match variables

--- plugins/event-messages.js
+++ plugins/event-messages.js
@@ -14,6 +14,13 @@ em.init = function (client, config) {
     client.addListener('kick', function (channel, nick, by, reason, message) {
         var messages = em.getKickMessages(config);
         var msg      = em.getRandomMessage(messages);
+            msg      = em.replaceVariables(msg, {
+                nick: nick,
+                channel: channel,
+                by: by,
+                reason: reason,
+                message: message
+        });
         
         client.say(channel, msg);
     });
@@ -21,13 +28,34 @@ em.init = function (client, config) {
     client.addListener('quit', function (channel, nick, by, reason, message) {
         var messages = em.getQuitMessages(config);
         var msg      = em.getRandomMessage(messages);
+            msg      = em.replaceVariables(msg, {
+                nick: nick,
+                channel: channel,
+                by: by,
+                reason: reason,
+                message: message
+        });
         
         client.say(channel, msg);
     });
 };
 
+em.replaceVariables = function (msg, kickInfo) {
+    var newMsg = '';
+    
+    newMsg = msg.replace(new RegExp('\\$nick', 'g'), kickInfo.nick);
+    newMsg = newMsg.replace(new RegExp('\\$channel', 'g'), kickInfo.channel);
+    newMsg = newMsg.replace(new RegExp('\\$by', 'g'), kickInfo.by);
+    newMsg = newMsg.replace(new RegExp('\\$reason', 'g'), kickInfo.reason);
+    newMsg = newMsg.replace(new RegExp('\\$message', 'g'), kickInfo.message);
+    
+    return newMsg;
+};
+
 em.getRandomMessage = function (messages) {
-    return messages[Math.floor(Math.random() * messages.length)];
+    var msg = messages[Math.floor(Math.random() * messages.length)];
+
+    return msg;
 };
 
 em.getKickMessages = function (cfg) {
