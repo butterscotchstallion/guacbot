5f69b56df0dc5fea6587041057758fb657866516
Your Name <bill@prgmrbill.com>
2013-05-21 02:46:50 -0400

    Adds note plugin Adds tests for note plugin

---
+++ plugins/note.js
@@ -0,0 +1,80 @@
+/**
+ * note - leave a note for someone special!
+ *
+ */
+"use strict";
+
+var moment   = require('moment');
+var parser   = require('../lib/messageParser');
+var ignore   = require('./ignore');
+
+var note     = {
+    notes: []
+};
+
+note.init = function (client) {
+    client.addListener('message#', function (nick, channel, text, message) {
+        var isAddressingBot = parser.isMessageAddressingBot(text, client.config.nick);
+        
+        if (isAddressingBot && !ignore.isIgnored(message.user + '@' + message.host)) {
+            var words     = parser.splitMessageIntoWords(text);
+            var command   = words[1];
+            var recipient = words[2];
+            var nMessage  = words.slice(3).join(' ');
+            
+            if (command === 'note') {
+                if (recipient && nMessage) {
+                
+                    note.add({
+                        nick: nick,
+                        channel: channel,
+                        timestamp: moment(),
+                        message: nMessage
+                    }, 
+                    function () {
+                        client.say(channel, 'k');
+                    },
+                    function () {
+                        client.say(channel, 'too many notes!!');
+                    });
+                    
+                } else {
+                    client.say(channel, 'does not compute');
+                }
+            }
+        }
+    }
+};
+
+note.get = function (nick, channel) {
+    return note.hasNotes(nick, channel);
+};
+
+note.add = function (n, successCallback, errBack) {
+    if (!note.hasNotes(n.nick, n.channel)) {
+        note.notes.push(n);
+        
+        if (successCallback) {
+            successCallback();
+        }
+        
+    } else {
+        if (errBack) {
+            errBack();
+        }
+    }
+};
+
+note.hasNotes = function (nick, channel) {
+    var nlen = note.notes.length;
+    
+    for (var j = 0; j < nlen; j++) {
+        if (note.notes[j].nick === nick && note.notes[j].channel === channel) {
+            return note.notes[j];
+        }
+    }
+    
+    return false;
+};
+
+module.exports = note;
---
+++ test/plugins/noteTest.js
@@ -0,0 +1,65 @@
+/**
+ * note tests
+ *
+ */
+"use strict";
+
+var moment    = require('moment');
+var assert    = require("assert");
+var note      = require('../../plugins/note');
+
+describe('note.get', function () {
+    beforeEach(function () {
+        note.notes = [];
+    });
+    
+    it('should get a note after adding it', function () {
+        var input = {
+            nick: 'biggie',
+            message: 'I live for the funk, I die for the funk',
+            channel: '#guacamole',
+            timestamp: moment()
+        };
+        
+        note.add(input);
+        
+        var actual = note.get(input.nick, input.channel);
+        
+        assert.deepEqual(input, actual);
+    });
+});
+
+describe('note.add', function () {
+    beforeEach(function () {
+        note.notes = [];
+    });
+    
+    it('should add a note', function () {
+        var n = {
+            nick: 'billulum',
+            message: 'biggie small is the illest',
+            timestamp: moment(),
+            channel: '#guacamole'
+        };
+        
+        note.add(n);
+        
+        assert.equal(note.notes.length, 1);
+        assert.deepEqual(note.notes[0], n);
+    });
+    
+    it('should add at most one from the same channel/nick', function () {
+        var n = {
+            nick: 'billulum',
+            message: 'biggie small is the illest',
+            timestamp: moment(),
+            channel: '#guacamole'
+        };
+        
+        note.add(n);
+        note.add(n);
+        
+        assert.equal(note.notes.length, 1);
+        assert.deepEqual(note.notes[0], n);
+    });
+});
