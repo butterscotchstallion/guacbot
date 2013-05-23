6c81365e5e11bc0893192e8e539065df4ee61dc9
Your Name <bill@prgmrbill.com>
2013-05-23 09:03:28 -0400

    Adds tests for admin plugin

---
+++ test/plugins/adminTest.js
@@ -0,0 +1,22 @@
+/**
+ * admin tests
+ *
+ */
+"use strict";
+
+var moment    = require('moment');
+var assert    = require("assert");
+var admin     = require('../../plugins/admin');
+
+describe('translate 5m to milliseconds', function () {
+    
+    it('should translate 5m to ms', function () {
+        var input    = '5m';
+        var expected = 300000;
+        var actual   = admin.timeToMilliseconds(input);
+        
+        assert.equal(expected, actual);
+    });
+    
+    
+});
