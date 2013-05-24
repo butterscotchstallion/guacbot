7f713bd179ef196b8b44f32f6601a7399648af98
Your Name <bill@prgmrbill.com>
2013-05-24 19:26:24 -0400

    Refactors/fixes messageParser.isAddressingBot to only return true when the first word of the message is equal to the bot's nick, excluding trailing characters like colons or commas to accomodate the common tab completion scenario "bot-nick:"
    Adds test to make sure all of the above works

---
+++ test/lib/messageParserTest.js
@@ -0,0 +1,28 @@
+/**
+ * messageParser test
+ *
+ */
+"use strict";
+
+var assert        = require("assert");
+var messageParser = require('../../lib/messageParser');
+ 
+describe('messageParser.isMessageAddressingBot', function () {
+    it('should not be true when first word contains nick but isnt nick', function () {
+        var expected = false;
+        var input    = 'sanitary napkin';
+        var nick     = 'n';
+        var actual   = messageParser.isMessageAddressingBot(input, nick);
+        
+        assert.equal(expected, actual);
+    });
+    
+    it('should be true when first word is the nick with a single character after it', function () {
+        var expected = true;
+        var input    = 'n:';
+        var nick     = 'n';
+        var actual   = messageParser.isMessageAddressingBot(input, nick);
+        
+        assert.equal(expected, actual);
+    });
+});
