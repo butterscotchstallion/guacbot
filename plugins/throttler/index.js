d2a7a1327f02132a504619aea4507315b7332389
Your Name <bill@prgmrbill.com>
2013-05-25 21:05:36 -0400

    Removes debugging code

--- plugins/throttler/index.js
+++ plugins/throttler/index.js
@@ -26,8 +26,6 @@ throttler.init = function (client) {
  */
 throttler.add = function (info) {
     if (!throttler.isThrottled(info.hostmask)) {
-        console.log('not throttled');
-        
         if (typeof(info.lastCommandTimestamp) === 'undefined') {
             info.lastCommandTimestamp = moment();
         }
@@ -47,8 +45,6 @@ throttler.add = function (info) {
             ts    = moment();
         }
         
-        console.log('iterating');
-        
         // Iterate all masks and update last command time
         for (var j = 0; j < mlen; j++) {
             if (masks[j] && minimatch(info.hostmask, masks[j].hostmask)) {            
@@ -57,14 +53,11 @@ throttler.add = function (info) {
                 if (typeof(masks[j].commandCount) === 'undefined') {
                     masks[j].commandCount         = 0;
                 } else {
-                    console.log('incrementing');
                     masks[j].commandCount        += 1;
                 }
             }
         }
     }
-    
-    //console.log(throttler.hostmasks);
 };
 
 throttler.isThrottled   = function (hostmask) {
@@ -81,7 +74,7 @@ throttler.isThrottled   = function (hostmask) {
     for (var j = 0; j < mlen; j++) {
         // First match hostmask
         if (masks[j].hostmask && minimatch(hostmask, masks[j].hostmask)) {
-            console.log('match: ' + masks[j].hostmask + ' == ' + hostmask);
+            //console.log('match: ' + masks[j].hostmask + ' == ' + hostmask);
             
             // Now check commandCount and calculate the amount of time it's been
             // since the last command was executed
@@ -92,12 +85,14 @@ throttler.isThrottled   = function (hostmask) {
             timeDiff        = moment().diff(masks[j].lastCommandTimestamp);
             minTimeExceeded = timeDiff > 1;
             
+            /*
             console.log(' ');
             console.log(' ');
             console.log('timediff:', timeDiff);
             console.log('cmds (' + maxCmds + '):', masks[j].commandCount);
             console.log('time (' + minTimeValue + minTimeUnit + '):', masks[j].commandCount);
             console.log(' ');
+            */
             
             if (maxCmdsExceeded && minTimeExceeded) {
                 isThrottled = true;
