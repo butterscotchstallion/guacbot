94fd7cf28344b144dc8942b72b57a3b2ad398ea4
Your Name <bill@prgmrbill.com>
2013-06-02 11:18:53 -0400

    Initialize scrambler before reading file

--- test/plugins/addWordsToDBTest.js
+++ test/plugins/addWordsToDBTest.js
@@ -6,6 +6,10 @@ var assert = require("assert");
 var p      = require('../../plugins/scrambler/addWordsToDB');
 
 describe('p.readWordsFile', function () {
+    beforeEach(function () {
+        scrambler.init();
+    });
+    
     it('should read the file', function (done) {
         p.readWordsFile('../plugins/scrambler/long-with-consonants.txt', function (words) {
             console.log(words);
